b60702deb9acd6ccb136ba077185cb2445be27ac
willen <willen@corp.kucoin.com>
2022-01-27 14:52:06 +0800

    v2

--- src/core.js
+++ src/core.js
@@ -4,28 +4,23 @@ require('@babel/register');
 const path = require('path');
 const fs = require('fs');
 const app = require('express')();
-const bodyParser = require('body-parser');
-const upload = require('multer')();
-const axios = require('axios');
-const querystring = require('querystring');
 const pathToRegexp = require('path-to-regexp');
+const _ = require('lodash');
+const { createProxyMiddleware, responseInterceptor } = require('http-proxy-middleware');
 const ROOT_DIR = path.join(__dirname, '../../../');
 let PORT;
 let MOCK_DIR;
 let LISTEN;
+let DEV_HOST;
 let HEADERS;
 let USE_TEMP_MOCK;
 let apiList = {};
 let fallbackApiList = {};
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(upload.array());
-
 // 获取配置
 function getConfig() {
   let config = {};
-  const {configPath, port, mockPath, listen, useTempMock} = process.env;
+  const { configPath, port, mockPath, listen, useTempMock } = process.env;
   try {
     const res = require(path.join(ROOT_DIR, configPath || '/.mock-server.js'));
     config = res.default || res;
@@ -37,14 +32,15 @@ function getConfig() {
   LISTEN = listen || config.listen;
   HEADERS = config.headers || {};
   USE_TEMP_MOCK = !!useTempMock || config.useTempMock;
+  DEV_HOST = config.devHost;
 }
 
 // 初始化mock目录api
 function collectApi() {
   apiList = {};
-  fs.readdir(MOCK_DIR, function (err, files) {
+  fs.readdir(MOCK_DIR, function(err, files) {
     if (!err) {
-      files.forEach(function (filename) {
+      files.forEach(function(filename) {
         if (/^.*js$/.test(filename)) {
           const filePath = path.join(MOCK_DIR, filename);
           delete require.cache[filePath];
@@ -61,9 +57,9 @@ function collectApi() {
 // 初始化mock/_temp目录api
 function collectFallbackApi() {
   fallbackApiList = {};
-  fs.readdir(path.join(MOCK_DIR, '/_temp'), function (err, files) {
+  fs.readdir(path.join(MOCK_DIR, '/_temp'), function(err, files) {
     if (!err) {
-      files.forEach(function (filename) {
+      files.forEach(function(filename) {
         if (/^.*js$/.test(filename)) {
           const filePath = path.join(MOCK_DIR, '/_temp', filename);
           delete require.cache[filePath];
@@ -77,10 +73,9 @@ function collectFallbackApi() {
   });
 }
 
-
 // 监听mock一级目录文件变化
 function watchMockFiles() {
-  fs.watch(MOCK_DIR, function (event, filename) {
+  fs.watch(MOCK_DIR, function(event, filename) {
     if (/^.*js$/.test(filename)) {
       console.log(filename, 'update success!');
       collectApi();
@@ -90,109 +85,89 @@ function watchMockFiles() {
 
 // Listen模式
 function listenMode() {
-  app.all('/*', async function (req, res) {
-    const {method, headers, query, _parsedUrl, body} = req;
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', headers['origin']);
-    res.header('Access-Control-Allow-Headers', headers['access-control-request-headers']);
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-    if (method === 'OPTIONS') res.sendStatus(200);
-    else {
-      const url = _parsedUrl.pathname;
-      let data = body;
-      if (headers['content-type'] && headers['content-type'].indexOf('application/json') < 0) {
-        data = body ? querystring.stringify(body) : undefined;
-      }
-      try {
-        console.log('----------------------------------------------');
-        console.log(`${method} ${url}`);
-        console.log('query', query);
-        console.log('body', data);
-
-        const result = await axios({
-          baseURL: LISTEN,
-          url,
-          method,
-          headers: {...headers, ...HEADERS},
-          params: query,
-          data,
-        });
-        res.send(result.data);
-        writeResponseData(method, url, result.data);
-      } catch (e) {
-        console.log(e);
-        res.sendStatus(500);
-      }
-    }
-  });
+  app.use(
+    '/*',
+    createProxyMiddleware({
+      target: LISTEN,
+      changeOrigin: true,
+      secure: false,
+      selfHandleResponse: true,
+      onProxyReq(proxReq, req, res) {
+        proxReq.setHeader('origin', LISTEN);
+      },
+      onProxyRes: responseInterceptor(async (responseBuffer, proxyRes, req, res) => {
+        const origin = req.headers.origin || DEV_HOST;
+        res.setHeader('Access-Control-Allow-Credentials', true);
+        res.setHeader('Access-Control-Allow-Origin', origin);
+        // 优先获取mock数据
+        const mockData = await getMockData(req, res);
+        if (!!mockData) {
+          res.statusCode = 200;
+          return JSON.stringify(mockData);
+        }
+        writeResponseData(req.method, req.path, responseBuffer.toString('utf-8'));
+        return responseBuffer;
+      }),
+    }),
+  );
 }
 
-// Mock模式
-function mockMode() {
-  app.all('/*', async function (req, res) {
-    const {method, headers, query, _parsedUrl, body} = req;
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', headers['origin']);
-    res.header('Access-Control-Allow-Headers', headers['access-control-request-headers']);
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-
-    if (method === 'OPTIONS') res.sendStatus(200);
-    else {
-      const url = _parsedUrl.pathname;
-
-      console.log('----------------------------------------------');
-      console.log(`${method} ${url}`);
-      console.log('query', query);
-      console.log('body', body);
-
-      let responseBody = null;
-      [apiList, fallbackApiList].forEach(list => {
-        if (!responseBody) {
-          for (let one in list) {
-            if (list.hasOwnProperty(one)) {
-              let split = one.split(' ');
-              if (split.length === 2) {
-                const reg = pathToRegexp(split[1]);
-                if (reg.exec(url) && split[0].toUpperCase() === method.toUpperCase()) {
-                  responseBody = list[one];
-                }
-              } else {
-                const reg = pathToRegexp(split[0]);
-                if (reg.exec(url)) {
-                  responseBody = list[one];
-                }
-              }
+// 获取MockData
+async function getMockData(req, res) {
+  const { method, headers, query, path, body } = req;
+  let responseBody = null;
+  [apiList, fallbackApiList].forEach(list => {
+    if (!responseBody) {
+      for (let one in list) {
+        if (list.hasOwnProperty(one)) {
+          let split = one.split(' ');
+          if (split.length === 2) {
+            const reg = pathToRegexp(split[1]);
+            if (reg.exec(path) && split[0].toUpperCase() === method.toUpperCase()) {
+              responseBody = list[one];
+            }
+          } else {
+            const reg = pathToRegexp(split[0]);
+            if (reg.exec(path)) {
+              responseBody = list[one];
             }
           }
         }
-      });
-      if (responseBody) {
-        if (typeof responseBody === 'function') {
-          try {
-            const data = await responseBody(req, res);
-            res.send(data);
-          } catch (e) {
-            res.sendStatus(500);
-          }
-        } else {
-          res.send(responseBody);
-        }
-      } else {
-        res.sendStatus(404);
       }
     }
   });
+  if (responseBody) {
+    if (typeof responseBody === 'function') {
+      try {
+        const data = await responseBody(req, res);
+        return data;
+      } catch (e) {
+        return { success: false, msg: 'mock data format failed' };
+      }
+    } else {
+      return responseBody;
+    }
+  } else {
+    return false;
+  }
 }
 
 // 将请求数据写入临时文件
 function writeResponseData(method, url, data) {
-  const filePath = path.join(MOCK_DIR, `_temp/${url.replace(/\//g, '.')}${method === 'GET' ? '' : '-' + method}.js`);
-  fs.writeFile(filePath, `module.exports = ${JSON.stringify({[`${method} ${url}`]: data})}`, (err) => {
-    if (err) {
-      console.log(err);
-    }
-  });
+  if (!data || !url) return;
+  const filePath = path.join(
+    MOCK_DIR,
+    `_temp/${url.replace(/\//g, '.')}${method === 'GET' ? '' : '-' + method}.js`,
+  );
+  fs.writeFile(
+    filePath,
+    `module.exports = ${JSON.stringify({ [`${method} ${url}`]: JSON.parse(data) })}`,
+    err => {
+      if (err) {
+        console.log(err);
+      }
+    },
+  );
 }
 
 // 初始化服务
@@ -201,20 +176,15 @@ function init() {
   getConfig();
 
   // 写入_temp目录
-  fs.mkdir(path.join(MOCK_DIR, '_temp'), {recursive: true}, (err) => {
-  });
+  fs.mkdir(path.join(MOCK_DIR, '_temp'), { recursive: true }, err => {});
 
-  if (LISTEN) {
-    listenMode();
-  } else {
-    collectApi();
-    USE_TEMP_MOCK && collectFallbackApi();
-    watchMockFiles();
-    mockMode();
-  }
+  collectApi();
+  USE_TEMP_MOCK && collectFallbackApi();
+  watchMockFiles();
+  listenMode();
 
-  app.listen(PORT, function () {
-    console.log(`Mock server listening on port: ${PORT}, Mode: ${LISTEN ? '\033[34m ListenMode \033[0m' : '\033[34m MockMode \033[0m'}`);
+  app.listen(PORT, function() {
+    console.log(`Mock server listening on port: ${PORT}`);
   });
 }
 
